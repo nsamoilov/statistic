d17ae6f88199b18c34310a1213c7d97de9d04548
NSamoilove <91327378+nsamoilov@users.noreply.github.com>
2022-07-12 23:04:51 +0300

    COV-Скрипт для визуализации данных из статистики

---
+++ g0.py
@@ -0,0 +1,52 @@
+import csv
+import matplotlib.pyplot as plt
+from itertools import groupby
+import pandas as pd
+
+class Data():
+    def __init__(self, country, data, case):
+        self.country = country
+        self.data = data
+        self.case = case
+
+allDataList = list()
+with open('/Users/nsamoilove/Desktop/coronavirus_dataset.csv', newline='') as File:  
+    reader = csv.reader(File)
+    for row in reader: 
+        data = Data(row[1],row[4],row[5])
+        allDataList.append(data)
+
+plt.figure(figsize=(25,50), dpi= 80)
+plt.title('График SARS-COV19')
+plt.grid(True)
+#country = list()
+for countryName, countryGroup in groupby(allDataList, lambda x: x.country):
+    cases = dict()
+    values = list()
+    date = ['2020-02']
+    #country.append(countryName)
+    for timeInfo in countryGroup:
+        #print(countryName,'/', timeInfo.data, '/', timeInfo.case)
+        if timeInfo.data[0:7] == '2020-01':
+            continue #Пропускаем первый месяц - в нём слишком мало данных
+        else:
+            if timeInfo.data[0:7] in date:
+                try:
+                    cdata = int(timeInfo.case)
+                    values.append(cdata)
+                except:
+                    continue
+            else:
+                if len(values) == 0:
+                    del values
+                else:
+                    cases[date[-1]] = sum(values)
+                    values = list()
+                    date.append(timeInfo.data[0:7])
+    plt.plot(cases.keys(), cases.values(), label=countryName, alpha=.7)
+    #print("Next country")
+country = country[1:]
+plt.xlabel("Дата", fontsize=44, fontweight="bold")
+plt.ylabel("Значения", fontsize=44, fontweight="bold")
+#plt.axis([1,200000,2,200])
+plt.show()
