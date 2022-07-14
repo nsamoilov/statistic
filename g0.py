679b57900f013f0f50212822caa3af291b4d1742
NSamoilove <91327378+nsamoilov@users.noreply.github.com>
2022-07-14 16:16:05 +0300

    Обновленный алгоритм

--- g0.py
+++ g0.py
@@ -1,7 +1,9 @@
 import csv
+import numpy as np 
 import matplotlib.pyplot as plt
 from itertools import groupby
 import pandas as pd
+%matplotlib inline
 
 class Data():
     def __init__(self, country, data, case):
@@ -10,13 +12,17 @@ class Data():
         self.case = case
 
 allDataList = list()
-with open('/Users/nsamoilove/Desktop/coronavirus_dataset.csv', newline='') as File:  
+with open('/Users/nsamoilove/Desktop/coronavirus_dataset3.csv', newline='') as File:  
     reader = csv.reader(File)
+    firstInfoRow = True
     for row in reader: 
+        if firstInfoRow == True:
+            firstInfoRow = False
+            continue
         data = Data(row[1],row[4],row[5])
         allDataList.append(data)
 
-plt.figure(figsize=(25,50), dpi= 80)
+plt.figure(figsize=(25,50))
 plt.title('График SARS-COV19')
 plt.grid(True)
 #country = list()
@@ -43,10 +49,10 @@ for countryName, countryGroup in groupby(allDataList, lambda x: x.country):
                     cases[date[-1]] = sum(values)
                     values = list()
                     date.append(timeInfo.data[0:7])
-    plt.plot(cases.keys(), cases.values(), label=countryName, alpha=.7)
+    plt.plot(cases.keys(), cases.values(), label=countryName, linewidth=1.0)
     #print("Next country")
 country = country[1:]
 plt.xlabel("Дата", fontsize=44, fontweight="bold")
 plt.ylabel("Значения", fontsize=44, fontweight="bold")
-#plt.axis([1,200000,2,200])
+plt.legend()
 plt.show()
