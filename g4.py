cab732bbf70638e38530bff822fb01d8761cc3d4
NSamoilove <91327378+nsamoilov@users.noreply.github.com>
2022-07-18 20:52:28 +0300

    Древовидная карта

--- g4.py
+++ g4.py
@@ -1,26 +1,21 @@
-#pip install pandas
-#pip install deep-translator
-import pandas as pd
-from deep_translator import PonsTranslator
-#Чтение данных из git репозитория
-url = 'https://raw.githubusercontent.com/nsamoilov/statistic/main/coronavirus_dataset4.csv'
-df = pd.read_csv(url, delimiter=',')
-#Группировка по странам, так как одной стране отведено 114 строк в файле
-group_data = df.groupby(['Country.Region'])
-#Сумма всех элементов столбца 'cases' с привязкой к группе (одной стране)
-cases_data = group_data.agg({'cases': ['sum']})['cases']['sum']
-#Формирование списка стран, перебор двумерного массива для получения ключа
-countries_data = list()
-for key, item in group_data:
-    #Перевод ключа с анлийского на русский с помощью словаря Pons (https://ru.pons.com/перевод)
-    translated_word = PonsTranslator(source='english', target='russian').translate(key, return_all=False)
-    #Добавление в список строки "страна (общее количество заболевших)"
-    countries_data.append(translated_word+'('+str(cases_data[key])+')')
+import pandas as pd                                                                                         #pip install pandas
+from deep_translator import PonsTranslator                                                                  #pip install deep-translator
+import matplotlib.pyplot as plt                                                                             #pip install matplotlib
+import squarify                                                                                             #pip install squarify
 
-#Формирования различных цветов для отображения    
-colors = [plt.cm.Spectral(i/float(len(countries_data))) for i in range(len(countries_data))]
-#Отрисовка фигуры
-plt.figure(figsize=(12,8), dpi= 80)
+url = 'https://raw.githubusercontent.com/nsamoilov/statistic/main/coronavirus_dataset3.csv'                 #ссылка git репозитория
+df = pd.read_csv(url, delimiter=',')                                                                        #Чтение данных из git репозитория
+group_data = df.groupby(['Country.Region'])                                                                 #Группировка по странам, так как однйо стране отведено 114 строк в файле
+cases_data = group_data.agg({'cases': ['sum']})['cases']['sum']                                             #Сумма всех элементов столбца 'cases' с привязкой к группе (одной стране)
+
+countries_data = list()                                                                                     #Формирования списка стран, перебор двумерного массива для получения ключа
+for key, item in group_data:                                                                                #Перевод ключа с анлийского на русский с помощью словаря Pons (https://ru.pons.com/перевод)
+    translated_word = PonsTranslator(source='english', target='russian').translate(key, return_all=False)
+    countries_data.append(translated_word+'('+str(cases_data[key])+')')                                     #Добавление в список строки типа: страна - общее количество заболевших
+   
+colors = [plt.cm.Spectral(i/float(len(countries_data))) for i in range(len(countries_data))]                #Формирования различных цветов для отображения 
+
+plt.figure(figsize=(12,8), dpi= 80)                                                                         #Отрисовка фигуры
 squarify.plot(sizes=cases_data, label=countries_data, color=colors, alpha=.8)
 plt.title('Количество заболевших по странам')
 plt.axis('off')
